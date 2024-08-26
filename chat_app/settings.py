0af54dcc32f1fbb70a1512874741fb9dc6dd3312
Mark Valentino <mike9377mike9377@gmail.com>
2024-08-26 01:38:46 -0400

    fixed stylesheet not loading

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qnw5ugd&4owi^*#5*2)9q4@p_ye#++*c1jwdvlo!!@g%lqrk(f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
